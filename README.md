bd0c66cca6b7901bd7dc1e1aa8430ea34fcc5608
derrad <radivoje@deretic.net>
2017-02-13 20:20:05 +0100

    Opet sa lokala novi direktorij

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # zadruga
 Node server za zadruge. Baza - mongodb
 Provera kako da ubacimo na server projekat koji imam.
+Idemo sa lokala nesto mi ne prabacuje na server
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Server za zadrugu nodejs
+
 var express            = require('express'),
      app               = express(),
      bodyParser        = require('body-parser'),
